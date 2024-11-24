c69fe62376585cca5ebe5e87199f77803622eb15
Saurav Suman <134825390+Saurav09s@users.noreply.github.com>
2024-11-24 10:16:41 +0530

    Update MergeSort.c

--- LAB 10/MergeSort.c
+++ LAB 10/MergeSort.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Merge(int a[],int n)
+void Merge(int a[],int l, int h, int mid)
+{
+    int i = l, k = l, j = mid+1, B[1000];
+    while(i<=mid&&j<=h)
+    {
+        if(a[i]<a[j])
+            B[k++]=a[i++];
+        else
+            B[k++]=a[j++];
+    }
+    for(;i<=mid;i++)
+        B[k++]=a[i];
+    for(;j<=h;j++)
+        B[k++]=a[j];
+    for(i=0;i<=h;i++)
+        a[i]=b[i];      
+}
+void MergeSort(int a[],int n)
 {
     
-}
-void Sort(int a[],int n)
-{
-
 }
